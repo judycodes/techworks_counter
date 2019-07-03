f1fdce8f075446567e076551e86df6a276d6ee00
Judy <codelinked@gmail.com>
2019-07-03 17:39:13 -0400

    counter template

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React, { Component } from 'react'
+//imrc for react component shortcut through react snippet package
+//imr for react without class components through react snippet package
+//========================================================================
+//Below writes hello world yay! on page 
+//class HelloWorld extends Component {
+//   render() {
+//     return (
+//       <div>Hello World Yay!</div>
+//     )
+//   }
+// }
 
-function App() {
+// export default HelloWorld;
+//always remember to export the class
+//==========================================================================
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  constructor () {
+    super();
+    this.state = {
+      //initalizes counter at zero....colon used with objects
+      counter: 0,
+
+
+    }
+  }
+
+render() {
+  //shows that the counter is displaying properly at initialized zero
+  console.log("This is line 32 representing state", this.state.counter); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <div className="test"></div>
-      let test = document.querySelector(".test");
-      
+    <div className = "container">
+       <div className="navbar">Counter.js</div>
+       <div className="counter">
+         <h1>What do we put here?</h1>
+         <button type="button">Increment</button>
+         <button type="button">Decrement</button>
+       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+}
+
+export default Counter;
+//alt shift down/up copies current line below or above depending on copy placement desires
