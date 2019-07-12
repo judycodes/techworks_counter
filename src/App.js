1799e233ea66a442cb12544e70d05dd12c8c43a7
Judy <codelinked@gmail.com>
2019-07-12 16:57:49 -0400

    toggle text fixed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class Counter extends Component {
     super();
     this.state = {
       counter: 0,
-      toggle: true,
-      dblOrSingle: 'Change to + / -  2'
+      toggle: true, //initialized at single incremements
+      
     };
   }
 
@@ -33,7 +33,7 @@ handleIncrement = () => {//method for button to run when increment button is cli
     if (this.state.toggle) {
       this.setState({ counter: this.state.counter - 1 }); //decrements by 1 depending on state of toggle
     } else {
-      this.setState({ counter: this.state.counter - 2 }); //decrements by 2 depending on state of toggle
+      this.setState({ counter: this.state.counter - 2}); //decrements by 2 depending on state of toggle
     }
   } else { //state counter is less than zero causes an alert pops up and resets the state of counter back to zero
     this.handleReset();
@@ -49,15 +49,13 @@ handleReset = () => { //method that causes state of counter to be zero
 };
 
 handleToggle = () => { //toggles between incrementing/decrementing by 1 or 2
+  
   if (this.state.toggle) { //condition is set to this.state.toggle which is initialized at true
     this.state.toggle = !this.state.toggle; //changes current state of toggle - true/false, true is at increment/decrement of 1, false is at increment/decrement of 2
-     
-    if (this.state.toggle) {
-      this.setState({ dblOrSingle : 'Change to + / -  2'});
-      
-    } else {
-      this.setState({ dblOrSingle : 'Change to + / -  2'});
-    }
+    
+  } else {
+    this.state.toggle = true;
+    
   }
 };
 
@@ -65,9 +63,6 @@ handleToggle = () => { //toggles between incrementing/decrementing by 1 or 2
 //=======[RENDER]============
 
 render() {
-  let dblOrSingle;
-
-
   return (
 
     <div className = "container">
@@ -80,7 +75,7 @@ render() {
          <button type="button" onClick = {this.handleIncrement}> + </button>
          <button type="button" onClick = {this.handleDecrement}> - </button>
          <button type="button" onClick = {this.handleReset}>Reset</button>
-         <button type="button" onClick={this.handleToggle}>{dblOrSingle}</button>
+         <button type="button" onClick={this.handleToggle}>{this.state.toggle ? "Change to + / -  2" : "Back to + / -  1"}</button>
        </div>
     </div>
   );
