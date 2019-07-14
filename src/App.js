834672b705fe536b50ebb4ea358900700bb756ed
Judy <codelinked@gmail.com>
2019-07-14 12:18:23 -0400

    jump added

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import sheep from "./sheep.png";
 
 class Counter extends Component {
   constructor () {
     super();
+
     this.state = {
       counter: 0,
       toggle: true, //initialized at single incremements
-      
+      doublesAvailable: true, //incrementing/decrementing by 2 option is available/"true" at initialization
+      oneSheepImg: true
     };
+  
   }
 
 //=======[EVENTS]============
 handleIncrement = () => {//method for button to run when increment button is clicked
     //state change for incrementing within range max of 20
-    if (this.state.counter < 20) {
+
+    if (this.state.counter < 20) { //if state counter is less than 20, allow incrementing 
+    
       if (this.state.toggle) {
         this.setState({ counter: this.state.counter + 1 }); //increments by 1 depending on state of toggle
       } else {
         this.setState({ counter: this.state.counter + 2 }); //increments by 2 depending on state of toggle
       }
-    } else { //state counter is more than twenty causes an alert pops up and resets the state of counter back to zero
+
+    } else { //state counter is more than 20 causes an alert pops up and resets the state of counter back to zero
       this.handleReset();
-      alert('Your counter is out of number range! STARTING OVER!');
+      alert('SORRY, YOU DID NOT RAISE ENOUGH SHEEP FOR THIS SLEEPLESS NIGHT! COUNT THEM OVER AGAIN!');
     }
     
   };
  
- handleDecrement = () => {
+handleDecrement = () => {
    //state change for incrementing within range min of 0
-  if (this.state.counter > 0) {
+
+  if (this.state.counter > 0) { //if state counter is more than 0, allow decrementing
+
     if (this.state.toggle) {
       this.setState({ counter: this.state.counter - 1 }); //decrements by 1 depending on state of toggle
     } else {
       this.setState({ counter: this.state.counter - 2}); //decrements by 2 depending on state of toggle
     }
+
   } else { //state counter is less than zero causes an alert pops up and resets the state of counter back to zero
     this.handleReset();
-    alert('Your counter is out of number range! STARTING OVER!');
-    
+    alert('YOUR SHEEP RAN OFF RANGE! START COUNTING UP!');
   }
+
 };
 
-handleReset = () => { //method that causes state of counter to be zero
-    this.setState({
-      counter: 0
-    })
+handleReset = () => { //method that causes state of counter to return to zero
+
+  this.setState({
+    counter: 0
+  })
+
 };
 
-handleToggle = () => { //toggles between incrementing/decrementing by 1 or 2
+handleToggle = () => { //toggles between incrementing/decrementing by 1 or 2 and the text displayed in toggle button
   
-  if (this.state.toggle) { //condition is set to this.state.toggle which is initialized at true
-    this.state.toggle = !this.state.toggle; //changes current state of toggle - true/false, true is at increment/decrement of 1, false is at increment/decrement of 2
+  this.setState(
+    function(prevState) { //function passes prevState, which is current values of state properties. prevState starts at increments/decrements of 1
+      return {doublesAvailable: !prevState.doublesAvailable, //changes state properties and shows what option is available on toggle button: 
+        //if toggle button displays 'change to + / - 2', then counter is incrementing/decrementing by 1, if it displays 'back to 1', then counter is incrementing/decrementing by 2
+      toggle : !prevState.toggle}; //toggles between which increment/decrement option is available
+    });
+    //when toggle button is pressed/clicked, the state properties are changed to false
+    //so toggle initalized as true and incrementing/decrementing by 1, now is false and increments/decrements by 2
+    //AND toggle button initalized as true, not is false and displays the option to return to incrementing/decrementing by 1 
     
-  } else {
-    this.state.toggle = true;
-    
-  }
 };
 
 
@@ -65,18 +80,40 @@ handleToggle = () => { //toggles between incrementing/decrementing by 1 or 2
 render() {
   return (
 
-    <div className = "container">
-       <div className="navbar">Keep Count From 0 to 20</div>
-       <div className="counter">
-         
+    <div className = "container"> 
 
-         <h1>{this.state.counter}</h1> 
+      <div className="navbar">count sheep</div>  {/* COMPONENT TITLE */}
+      
+        <div className= "componentParts">
 
-         <button type="button" onClick = {this.handleIncrement}> + </button>
-         <button type="button" onClick = {this.handleDecrement}> - </button>
-         <button type="button" onClick = {this.handleReset}>Reset</button>
-         <button type="button" onClick={this.handleToggle}>{this.state.toggle ? "Change to + / -  2" : "Back to + / -  1"}</button>
-       </div>
+          <div className="counter"> {/* COUNTER COMPONENT WRAPPER*/}
+            
+            <h1>{this.state.counter}</h1> {/*DISPLAYS CURRENT COUNT*/}
+
+            <div className="buttonWrapper">
+
+              <button type="button" className = "plusBtn" onClick = {this.handleIncrement}> + </button> {/* Adds 1 or 2 depending on toggle state */}
+
+              <button type="button" className = "minusBtn"  onClick = {this.handleDecrement}> - </button> {/*Subtracts 1 or 2 depending on toggle state */}
+
+              <button type="button" className = "zeroBtn" onClick = {this.handleReset}>Reset</button> {/* Resets counter count to zero */}
+
+              <button type="button" className = "toggleBtn" onClick={this.handleToggle}>{this.state.doublesAvailable ? "Change" : "Back"}</button> {/* Allows toggle between adding/subtracting by 1 or 2 */}
+
+            </div>
+
+          </div>
+
+          <div className = "images">
+            <div className = "sheepContainer">
+            <img className = "sheep" src={sheep} alt="sheep image"/>
+            </div>
+
+            <img className = "fence" src="" alt="fence image"/>
+
+          </div>
+
+      </div> 
     </div>
   );
 }
