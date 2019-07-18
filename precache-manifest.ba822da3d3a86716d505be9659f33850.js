3475afbd4e2b0579be29d03fb1211dc288ee5919
Judy <codelinked@gmail.com>
2019-07-18 14:28:46 -0400

    Updates

---
+++ precache-manifest.ba822da3d3a86716d505be9659f33850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be08b8a0e216ee45594947034698e1fb",
+    "url": "/techworks_counter/index.html"
+  },
+  {
+    "revision": "e2100990efebb7e417c5",
+    "url": "/techworks_counter/static/css/main.39c088e5.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/techworks_counter/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "e2100990efebb7e417c5",
+    "url": "/techworks_counter/static/js/main.50f9b838.chunk.js"
+  },
+  {
+    "revision": "fdcc6cd63e2cc2ae4f57",
+    "url": "/techworks_counter/static/js/runtime~main.592e9664.js"
+  },
+  {
+    "revision": "2e2afe497d819ad3df909d8ade2d4021",
+    "url": "/techworks_counter/static/media/fence.2e2afe49.png"
+  },
+  {
+    "revision": "f9711231a746c1fb1c8e4d94880efea9",
+    "url": "/techworks_counter/static/media/sheep.f9711231.png"
+  }
+]);
